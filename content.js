604ac0a31c735fee841514e78c865d7de93644ca
ivanpetrovic89 <ivanpetrovic89@gmail.com>
2025-10-20 08:40:19 +0200

    Add selectors for Shorts shelf and videos in content.js

--- content.js
+++ content.js
@@ -4,6 +4,8 @@ const pageSelectors = {
     sidebar: 'a#endpoint.yt-simple-endpoint.style-scope.ytd-guide-entry-renderer[title="Shorts"]',
     channel: '#tabsContent > yt-tab-group-shape > div.yt-tab-group-shape-wiz__tabs > yt-tab-shape:nth-child(3)',
     video: 'ytd-reel-shelf-renderer',
+    shortsShelf: 'grid-shelf-view-model.ytGridShelfViewModelHost.ytd-item-section-renderer.ytGridShelfViewModelHostHasBottomButton',
+    shortsVideos: 'ytd-video-renderer:has(ytd-thumbnail-overlay-time-status-renderer[overlay-style="SHORTS"])'
 };
 
 // Set to track processed elements
